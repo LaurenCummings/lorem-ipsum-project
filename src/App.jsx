e3a971139a4f576c9c00292bb405011e61e74664
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-15 21:53:01 -0400

    Started adding state constants and html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world');
+  }
 
   return (
-    <div>
-      App
-    </div>
+    <section className="sectionCenter">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+      </form>
+    </section>
   )
 }
 
